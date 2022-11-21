eda08c3d4f810cc7747b90ebf52d39ff3097ac26
Ibrahim Diriye <ibrahim@diriye.org>
2022-11-21 04:22:50 +0300

    fucked up the py-script program, royally.

--- py-script.py
+++ py-script.py
@@ -1,7 +1,18 @@
-a=1
-b=2
-c=a+b
-d=input()
+a = 1
+b = 2
 
-print('input data for variable' + str(c))
+c = a + b
+
+z = c
+
+if z == c:
+    print("Lawd have mercy!")
+else print ("Ohhhhhhhhh")
+
+z -= 1
+
+if z == c:
+    print("Whoever threw that paper, your mom's a hoe!")
+else
+exit()
 
